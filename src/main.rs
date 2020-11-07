2fd446af233b5441a6362cca650080c0802bafac
Aditya Suresh <ethanaditya@gmail.com>
2020-11-07 13:00:41 -0700

    created rudimentary packet parser which prints ipv4 addresses in destination of packet headers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate pcap;
+extern crate etherparse;
+
+use pcap::Device;
+use etherparse::{SlicedPacket, InternetSlice};
 fn main() {
-    println!("Hello, world!");
+    let mut cap = Device::lookup()
+                         .unwrap()
+                         .open()
+                         .unwrap();
+
+    while let Ok(packet) = cap.next() {
+        match SlicedPacket::from_ethernet(packet.data) {
+            Err(value) => println!("Err {:?}", value),
+            Ok(value) => {
+                match value.ip {
+                    Some(InternetSlice::Ipv4(header)) => {
+                        println!("{:?}", header.source_addr())
+                    }
+                    Some(_) | None => (),
+                }
+            }
+        }
+    }
 }
