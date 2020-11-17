e72434bc34769567410fb9ad5995eacd88979ab0
DefunctLizard <grantshandy@gmail.com>
2020-11-17 14:44:35 -0700

    problems with pcap... added println! statements to try to debug.

--- src/ip.rs
+++ src/ip.rs
@@ -10,28 +10,35 @@ pub fn ipextract() {
 
     let path = &get_path();
 
-    let mut mapdata =
-        std::fs::File::create(&format!("{}ipmap.json", path)).expect(&format!("Couldn't create {}ipmap.json", path));
+
     let mut ip_index = HashSet::new();
     let mut latitude_index = HashSet::new();
     let mut longitude_index = HashSet::new();
 
+    println!("running device lookup");
     let mut cap = Device::lookup().unwrap().open().unwrap();
+    println!("finish device lookup");
 
     // Loop through each packet in the capture interface as an iterator until it returns an error.
     while let Ok(packet) = cap.next() {
+        println!("got a packet");
         match SlicedPacket::from_ethernet(packet.data) {
             Err(error) => Log::error(&error.to_string()),
             Ok(value) => match value.ip {
                 Some(InternetSlice::Ipv4(header)) => {
                     let current_ip = header.source_addr();
+                    println!("got an IP...");
                     if !ip_index.contains(&current_ip.to_string()) && !current_ip.is_private() {
                         ip_index.insert(current_ip.to_string());
                         // Run locator with the IP address, which returns Latitude and Longitude.
+
+                        println!("running the locator");
                         match Locator::get(current_ip.to_string()) {
                             Ok(ip) => {
+                                println!("ran the locator and it worked");
                                 if !latitude_index.contains(&ip.longitude) {
                                     if !longitude_index.contains(&ip.longitude) {
+                                        println!("unique IP location was found");
                                         let json = json!({
                                             "location": {
                                                 "ip": ip.ip,
@@ -40,10 +47,6 @@ pub fn ipextract() {
                                             }
                                         });
                                         println!("{} - {}", ip.ip, ip.city);
-                                        mapdata
-                                            .write_all(format!("\n{}", json).as_bytes())
-                                            .expect(&format!("Couldn't write to {}ipmap.json", path));
-
                                         longitude_index.insert(ip.longitude);
                                     }
                                     latitude_index.insert(ip.latitude);
@@ -65,6 +68,7 @@ pub fn ipextract() {
 
 // Set path for temporary file based on the operating system
 pub fn get_path() -> String {
+    println!("runing get_path()");
     if std::env::consts::OS == "windows" {
         return "%userprofile%\\AppData\\Local\\Temp\\".to_string();
     } else {
