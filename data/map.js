831666270b8819a2bd5d0d58436fbf0e4ec1e912
DefunctLizard <grantshandy@gmail.com>
2020-11-25 18:31:54 -0700

    add IP counter to site

--- data/map.js
+++ data/map.js
@@ -1,16 +1,12 @@
 var ipmap = L.map('mapid').setView([20, 0], 2);
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-  maxZoom: 18,
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-    'Imagery Â© <a href="https://www.mapbox.com/">Mapbox</a>',
-  id: 'mapbox/streets-v11',
-  tileSize: 512,
-  zoomOffset: -1
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(ipmap);
 
 var alreadyAdded = new Set();
+
+var ipIndex = 0;
 
 function addMarkers(jsonText) {
 	console.log(jsonText)
@@ -22,6 +18,11 @@ function addMarkers(jsonText) {
 		}
 
 		console.log(obj.ip);
+
+		ipIndex = ipIndex + 1;
+
+		document.getElementById("totalips").innerHTML = ipIndex + " Unique IPs";
+
 		L.marker([obj.latitude, obj.longitude]).addTo(ipmap)
 // .bindpopup(obj.ip);
 		alreadyAdded.add(obj.ip);
